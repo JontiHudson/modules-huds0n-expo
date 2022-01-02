6316b32a652dcf220c091a75578fa13970681ba1
Jonti Hudson <33636074+JontiHudson@users.noreply.github.com>
2022-01-02 18:15:21 +0000

    1.6.0-beta-1

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,4 @@
-export * from './moduleExports';
-export * from './themedComponents';
+export * from '@huds0n/react-native';
+export * from '@huds0n/expo-cache-image';
+export * from '@huds0n//expo-notification-manager';
+export * from '@huds0n/expo-orientation-manager';
