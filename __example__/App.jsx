be72206bd03cf8a10cc1d73a459245ac3c701f6b
Jonti Hudson <33636074+JontiHudson@users.noreply.github.com>
2022-01-13 15:24:49 +0000

    1.6.0-beta63

--- __example__/App.jsx
+++ __example__/App.jsx
@@ -1,33 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const react_1 = (0, tslib_1.__importDefault)(require("react"));
-const expo_1 = require("@huds0n/expo");
-const theme_1 = require("./src/theme");
+exports.useIsDarkMode = exports.isDarkMode = void 0;
+const react_1 = require("react");
+const react_native_1 = require("react-native");
 function App() {
-    (0, expo_1.useIsDarkMode)();
-    return (<expo_1.ScreenManager initialAppearance={{
-            get backgroundColor() {
-                return theme_1.colors.WARN;
-            },
-            get bottomBar() {
-                return theme_1.colors.PRIMARY;
-            },
-            get leftBar() {
-                return theme_1.colors.BLACK;
-            },
-            get rightBar() {
-                return theme_1.colors.BLACK;
-            },
-            get statusBar() {
-                return theme_1.colors.PRIMARY;
-            },
-        }}>
-      <expo_1.Toast>
-        <expo_1.InputManager>
-          
-        </expo_1.InputManager>
-      </expo_1.Toast>
-    </expo_1.ScreenManager>);
+    useIsDarkMode();
+    return <react_native_1.View />;
 }
 exports.default = App;
+function isDarkMode() {
+    return react_native_1.Appearance.getColorScheme() === "dark";
+}
+exports.isDarkMode = isDarkMode;
+function useIsDarkMode() {
+    const [isDark, setIsDark] = (0, react_1.useState)(() => {
+        return isDarkMode();
+    });
+    (0, react_1.useEffect)(() => {
+        const listener = () => {
+            setIsDark(isDarkMode());
+        };
+        react_native_1.Appearance.addChangeListener(listener);
+        return () => react_native_1.Appearance.removeChangeListener(listener);
+    }, []);
+    return isDark;
+}
+exports.useIsDarkMode = useIsDarkMode;
--- __example__/App.tsx
+++ __example__/App.tsx
@@ -1,62 +1,91 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Appearance, View } from "react-native";
 
-import {
-  Stack,
-  InputManager,
-  ScreenManager,
-  Toast,
-  useIsDarkMode,
-} from "@huds0n/expo";
+// import {
+//   Stack,
+//   InputManager,
+//   ScreenManager,
+//   Toast,
+//   // useIsDarkMode,
+// } from "@huds0n/expo";
 
 import { colors } from "./src/theme";
 
 export default function App() {
   useIsDarkMode();
 
-  return (
-    <ScreenManager
-      initialAppearance={{
-        get backgroundColor() {
-          return colors.WARN;
-        },
-        get bottomBar() {
-          return colors.PRIMARY;
-        },
-        get leftBar() {
-          return colors.BLACK;
-        },
-        get rightBar() {
-          return colors.BLACK;
-        },
-        get statusBar() {
-          return colors.PRIMARY;
-        },
-      }}
-    >
-      <Toast>
-        <InputManager>
-          {/* <Stack
-            navigator={Navigator}
-            screens={{
-              Application: <Application />,
-              Auth: <Auth />,
-              CardView: <CardView />,
-              Home: <Home />,
-              PhoneVerify: <PhoneVerify />,
-              SignUp: <SignUp />,
-              SplashScreen: <SplashScreen />,
-            }}
-            screenStyle={{
-              backgroundColor: colors.BACKGROUND,
-              shadowOffset: { width: 0, height: 0 },
-              shadowRadius: 10,
-              shadowOpacity: 0.25,
-              shadowColor: colors.BORDER,
-              zIndex: -1,
-            }}
-          /> */}
-        </InputManager>
-      </Toast>
-    </ScreenManager>
+  return <View />;
+
+  // return (
+  //   <ScreenManager
+  //     initialAppearance={{
+  //       get backgroundColor() {
+  //         return colors.WARN;
+  //       },
+  //       get bottomBar() {
+  //         return colors.PRIMARY;
+  //       },
+  //       get leftBar() {
+  //         return colors.BLACK;
+  //       },
+  //       get rightBar() {
+  //         return colors.BLACK;
+  //       },
+  //       get statusBar() {
+  //         return colors.PRIMARY;
+  //       },
+  //     }}
+  //   >
+  //     <Toast>
+  //       <InputManager>
+  //         {/* <Stack
+  //           navigator={Navigator}
+  //           screens={{
+  //             Application: <Application />,
+  //             Auth: <Auth />,
+  //             CardView: <CardView />,
+  //             Home: <Home />,
+  //             PhoneVerify: <PhoneVerify />,
+  //             SignUp: <SignUp />,
+  //             SplashScreen: <SplashScreen />,
+  //           }}
+  //           screenStyle={{
+  //             backgroundColor: colors.BACKGROUND,
+  //             shadowOffset: { width: 0, height: 0 },
+  //             shadowRadius: 10,
+  //             shadowOpacity: 0.25,
+  //             shadowColor: colors.BORDER,
+  //             zIndex: -1,
+  //           }}
+  //         /> */}
+  //       </InputManager>
+  //     </Toast>
+  //   </ScreenManager>
+  // );
+}
+
+export function isDarkMode() {
+  return Appearance.getColorScheme() === "dark";
+}
+
+export function useIsDarkMode() {
+  const [isDark, setIsDark] = useState(() => {
+    return isDarkMode();
+  });
+
+  useEffect(
+    () => {
+      const listener = () => {
+        setIsDark(isDarkMode());
+      };
+
+      Appearance.addChangeListener(listener);
+
+      return () => Appearance.removeChangeListener(listener);
+    },
+    []
+    // { layout: 'BEFORE' },
   );
+
+  return isDark;
 }
